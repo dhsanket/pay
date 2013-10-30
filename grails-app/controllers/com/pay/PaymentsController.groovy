f027424ddf7b0b6def68f75cf32398d1352f981f
dhsanket <sanketd367@gmail.com>
2013-10-30 20:01:38 +0000

    with comments and cleaner code

--- grails-app/controllers/com/pay/PaymentsController.groovy
+++ grails-app/controllers/com/pay/PaymentsController.groovy
@@ -4,30 +4,33 @@ class PaymentsController {
 	static defaultAction = "pay"
 	
 	def pay = {
-		if (params.from != null) {
+		if (params.from != null) {   //if params.from is not available then we won't know who the transactions history look up is for or who to make the payment from
 			log.error " params $params"
 			def fromAccount = Account.findByName(params.from)
 			log.error " transactions for $fromAccount.name $fromAccount.transactions"
 			
-			if(params.to != null){
+			if(params.to != null){ //We need to know who to make the payment to
 			def toAccount = Account.findByName(params.to)
 			log.error " paying to person $toAccount.name"
-			def result = fromAccount.makePayment(params.amount, toAccount)
+			def result = fromAccount.makePayment(params.amount, toAccount)  //payment is made and the boolean return value captured
 			if (result){
+				
+				//emails are sent to both parties
 				sendMail {
 					to toAccount.email
 					subject "You have recieved funds"
-					body ' You have recieved ' + params.amount + ' from ' + fromAccount.name
+					body ' You have recieved GBP' + params.amount + ' from ' + fromAccount.name
 				  }
 				sendMail {
 					to fromAccount.email
 					subject "You have sent funds"
-					body ' You have sent ' + params.amount + ' to ' + toAccount.name
+					body ' You have sent GBP' + params.amount + ' to ' + toAccount.name
 				  }
 			}
+			//return a model for a transaction
 			return [ paidFrom:fromAccount, result:result ]
 			}
-
+			//return a model for a transactions history lookup for the "pay.gsp"
 			return [ paidFrom : fromAccount]
 		}
 	}
@@ -36,6 +39,7 @@ class PaymentsController {
 		if (params.from != null) {
 			log.error "params $params"
 			def fromAccount = Account.findByName(params.from)
+			// return a model for a transactions history lookup from the "transacitons.gsp" 
 			model:[ paidFrom : fromAccount]
 		}
 	}
--- grails-app/domain/com/pay/Account.groovy
+++ grails-app/domain/com/pay/Account.groovy
@@ -1,7 +1,8 @@
 package com.pay
 
 class Account {
-    double balance = 200
+	// sample accounts Joe and Patty are created in the bootStap.groovy file 
+    double balance = 200  // every account starts with 200 balance
 	String name
 	String email
 	static hasMany = [ transactions: String ]
@@ -13,25 +14,33 @@ class Account {
 		}
 	
 	public boolean makePayment (String amountText, Account paidTo ) {
+		//Boolean allows other classes to figure out status of the transaction
 		boolean transactionConfirmation
+		
+		// amount is recieved as String and then converted to double. Would need more robust validation in case non numerical values are received
 		double amount = amountText.toDouble()
 		log.error " Amount for payment as double primitive $amount"
-		if (this.balance >= amount ) {
 		
+		//Only if funds are avilable, any payment can be made
+		if (this.balance >= amount ) {
+		//payment is made
 		this.balance = this.balance - amount
+		//payment is received
 		paidTo.balance = paidTo.balance + amount
 		
-		def transactionFrom =  amount + " was paid to " + paidTo.name		
+		//transactions are recorded as String
+		def transactionFrom =  amount + " was paid to " + paidTo.name + " --> New balance is " + this.balance		
 		this.addToTransactions(transactionFrom.toString())
 		
-		String transactionTo = amount + " was paid from " + this.name
+		String transactionTo = amount + " was paid from " + this.name + " --> New balance is " + paidTo.balance
 		paidTo.addToTransactions(transactionTo.toString())
 	
 		this.save(flush:true)
 		paidTo.save(flush:true)
 		
+		//transaction is successful
 		transactionConfirmation = true
-		} else return transactionConfirmation = false
+		} else return transactionConfirmation = false //transaction not successful
 	} 
 
 }
